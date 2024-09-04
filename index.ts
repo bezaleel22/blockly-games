9d9a85b35296557c039b034263abc8ae530bd7e4
Onojeta Brown <onosbrown.saved@gmail.com>
2024-09-04 11:14:05 +0100

    Initial commit (via bun create)

---
+++ index.ts
@@ -0,0 +1,15 @@
+const BASE_PATH = "./public";
+
+
+console.log(`Listening on http://localhost:3000...`);
+Bun.serve({
+  port: 3000,
+  async fetch(req) {
+    const filePath = BASE_PATH + new URL(req.url).pathname;
+    const file = Bun.file(filePath);
+    return new Response(file);
+  },
+  error() {
+    return new Response(null, { status: 404 });
+  },
+});
